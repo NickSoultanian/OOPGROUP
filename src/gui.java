8889038842d3256a71e2d599e33a38a54e6319df
jlcamacho-dev <josecamacho94@gmail.com>
2020-04-23 21:47:17 -0700

    added date picker/calendar starter code

--- src/gui.java
+++ src/gui.java
@@ -1,12 +1,14 @@
 // Class File for javafx GUI 
 
 // impors for program
+import java.util.Locale;
 import javafx.application.Application;
-import javafx.event.ActionEvent;
-import javafx.event.EventHandler;
+import javafx.geometry.HPos;
 import javafx.scene.Scene;
-import javafx.scene.control.Button;
-import javafx.scene.layout.StackPane;
+import javafx.scene.control.DatePicker;
+import javafx.scene.control.Label;
+import javafx.scene.layout.GridPane;
+import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
 // to run ensure to have the javafx sdk installed 
@@ -22,26 +24,65 @@ import javafx.stage.Stage;
 // - Jose L. Camacho 4-23-20 
 
 // purpose - boiler plate program to test and get javafx running 
+// sample code uncomment to test
+//public class gui extends Application {
+//    Button button;
+//
+//    public static void main(String[] args){
+//        launch(args);
+//    }
+//
+//    @Override
+//    public void start(Stage primaryStage) throws Exception{
+//        primaryStage.setTitle("Example Title");
+//        button = new Button();
+//        button.setText("Click me");
+//
+//        StackPane layout = new StackPane();
+//        layout.getChildren().add(button);
+//
+//        Scene scene = new Scene(layout, 300, 250);
+//        primaryStage.setScene(scene);
+//        primaryStage.show();
+//    }
+//}
+
+
 
 public class gui extends Application {
-    Button button;
 
-    public static void main(String[] args){
+    private Stage stage;
+    private DatePicker checkInDatePicker;
+    public static void main(String[] args) {
+        Locale.setDefault(Locale.US);
         launch(args);
     }
 
     @Override
-    public void start(Stage primaryStage) throws Exception{
-        primaryStage.setTitle("Example Title");
-        button = new Button();
-        button.setText("Click me");
+    public void start(Stage stage) {
+        this.stage = stage;
+        stage.setTitle("DatePickerSample ");
+        initUI();
+        stage.show();
+    }
 
-        StackPane layout = new StackPane();
-        layout.getChildren().add(button);
+    private void initUI() {
+        VBox vbox = new VBox(20);
+        vbox.setStyle("-fx-padding: 10;");
+        Scene scene = new Scene(vbox, 400, 400);
+        stage.setScene(scene);
 
-        Scene scene = new Scene(layout, 300, 250);
-        primaryStage.setScene(scene);
-        primaryStage.show();
+        checkInDatePicker = new DatePicker();
+
+        GridPane gridPane = new GridPane();
+        gridPane.setHgap(10);
+        gridPane.setVgap(10);
+
+        Label checkInlabel = new Label("Check-In Date:");
+        gridPane.add(checkInlabel, 0, 0);
+
+        GridPane.setHalignment(checkInlabel, HPos.LEFT);
+        gridPane.add(checkInDatePicker, 0, 1);
+        vbox.getChildren().add(gridPane);
     }
 }
-
