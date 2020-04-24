05a7bf2e805e1369935572cb8f9ed8a056c28f5a
jlcamacho-dev <josecamacho94@gmail.com>
2020-04-23 20:10:14 -0700

    edited gui class file with sample javafx code and instructions to get javafx up and running on intelij IDE 2020.1

--- src/gui.java
+++ src/gui.java
@@ -1,11 +1,47 @@
 // Class File for javafx GUI 
 
-class gui{
+// impors for program
+import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.StackPane;
+import javafx.stage.Stage;
 
+// to run ensure to have the javafx sdk installed 
+// go here https://gluonhq.com/products/javafx/ 
+// install latest version of javafx sdk which is 14 
+// extra folder into preferred directory on windows I put in C:/ 
+// on intelij add library and make sure it points the lib folder in the sdk fodler 
+// after that headover to file -> Edit Configueration.. 
+// in the text field labed "vim options:~ add this 
+// --module-path "C:\javafx-sdk-14.0.1\lib" --add-modules javafx.controls,javafx.fxml 
+// path in the quotes is provided you put the javafx sdk in the root directory if not just copy+paste 
+// the file path inbetween the qutoes 
+// - Jose L. Camacho 4-23-20 
 
-	// stub driver function to test 
-	public static void main(String args[]){
-		// beginning of driver
-		
-	}
+// purpose - boiler plate program to test and get javafx running 
+
+public class gui extends Application {
+    Button button;
+
+    public static void main(String[] args){
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) throws Exception{
+        primaryStage.setTitle("Example Title");
+        button = new Button();
+        button.setText("Click me");
+
+        StackPane layout = new StackPane();
+        layout.getChildren().add(button);
+
+        Scene scene = new Scene(layout, 300, 250);
+        primaryStage.setScene(scene);
+        primaryStage.show();
+    }
 }
+
