72ea009c04d2e36d88663ca548a323b401fb6c22
Nerses-Kazarian <53448266+Nerses-Kazarian@users.noreply.github.com>
2020-04-29 22:32:17 -0700

    Update Scheduler.java
    Altered the implementation of the Scheduler constructors, added getTask methods (incomplete), added addTask(), removeTask(), and altered getRoundedTime() subroutine.

--- src/Scheduler.java
+++ src/Scheduler.java
@@ -1,30 +1,66 @@
-
 public class Scheduler {
+
+    List<Task> tasks = new ArrayList<Tasks>();
+
+    List<Task> reccurringTasks = new ArrayList<Tasks>();
 
     /* 
     *  Time will be adjusted to the nearest fifteen minutes, with the type-handling done by the constructors   
     *  Example code for working with Scheduler in Main: "setStartTime = new Scheduler(this.getStartTime());"
-     */
+    */
 
     // Constructor handles a Task of type RecurringTask
-    private Scheduler(RecurringTask rt){
-        getRoundedStartTime(rt.getStartTime());
-        getRoundedStartTime(rt.getEndTime());
+    private Scheduler(RecurringTask rt){ 
+        addTask(rt);       
+        getRoundedTime(rt.getStartTime());
+        getRoundedTime(rt.getEndTime());
     }
 
     // Constructor handles a Task of type TransientTask
-    private Scheduler(TransientTask tt){        
-        getRoundedStartTime(tt.getStartTime());
-        getRoundedStartTime(tt.getEndTime());
+    private Scheduler(TransientTask tt){      
+        addTask(tt);  
+        getRoundedTime(tt.getStartTime());
+        getRoundedTime(tt.getEndTime());
     }    
 
     // Constructor handles a Task of type AntiTask
-    private Scheduler(AntiTask at){                        
-        getRoundedStartTime(at.getStartTime());
-        getRoundedStartTime(at.getEndTime());
+    private Scheduler(AntiTask at){
+        addTask(at);                        
+        getRoundedTime(at.getStartTime());
+        getRoundedTime(at.getEndTime());
+    }    
+    
+    private List<Task> getAllTasks(){
+        return tasks;
+    }
+
+    // TODO: Grab all Tasks for that Day, Month, Week
+    private List<Task> getTasksForDay(){
+        return tasks;        
+    }
+
+    private List<Task> getTasksForMonth(){
+        return tasks;
+    }
+
+    private List<Task> getTasksForWeek(){
+        return tasks;
     }    
 
-    // Round to Nearest 
+    private void getRecurringTasks(){            
+        reccuringTasks = checkForDuplicateReccuringTask(reccuringTasksTemp);    
+        return reccurringTasks;
+    }
+
+    private void removeTask(Task task){
+        tasks.remove(task);
+    }
+
+    private void addTask(Task task){
+        tasks.add(task);
+    }
+
+    // Round to nearest fifteen minutes
     private double roundNearestFifteenMinutes(double time){
         if (startTime < 15) {
             time = 0;
@@ -38,51 +74,35 @@ public class Scheduler {
     }
 
     // Value of Task must be modified to the nearest 15 minutes
-	private double getRoundedStartTime(double startTime){  
-
+	private double getRoundedTime(double time){  
         // Grab everything after the "." of the double value, i.e the minute value
-        String startTimeString = String.valueOf(startTime);
-        startTime = Double.parseDouble(startTimeString.substring(startTimeString.indexOf(".") + 1));                
-        return roundNearestFifteenMinutes(startTime);        
-
+        String timeString = String.valueOf(time);
+        time = Double.parseDouble(startTimeString.substring(timeString.indexOf(".") + 1));                
+        return roundNearestFifteenMinutes(time);        
     }
 
-    // Value of Task must be modified to the nearest 15 minutes
-	private double getRoundedEndDate(double endDate){
-
-        // Grab everything after the "." of the double value, i.e the minute value
-        String startTimeString = String.valueOf(startTime);
-        startTime = Double.parseDouble(startTimeString.substring(startTimeString.indexOf(".") + 1));  
-        return roundNearestFifteenMinutes(endDate);   
-
-    }
 
     /* 
     *  Can be used to check for conflicting times.
-    *  Takes two tasks.
+    *  Takes a Task List.
     *  Iterate with a for loop in Main, check all Tasks (Recurring, Transient) in the Calendar to see if that Task is a duplicate. 
     */
 
-    // Check newly created RecurringTask for a duplicate
-    private boolean checkForDuplicateReccuringTask(RecuringTask firstTask, RecuringTask secondTask){
-        if(firstTask.getStartTime() == secondTask.getStartTime()){
-            if(firstTask.getEndTime() == secondTask.getEndTime()){
-                if(firstTask.getDate() == secondTask.getDate()){
-                    if(firstTask.getYear() == secondTask.getYear()){
-                        return true;
-                    }
-                }
-            }
-        }
-    }
-
-    // Check newly created TransientTask for a duplicate
-    public boolean checkForDuplicateTransientTask(TransientTask firstTask, TransientTask secondTask){
-        if(firstTask.getStartTime() == secondTask.getStartTime()){
-            if(firstTask.getEndTime() == secondTask.getEndTime()){
-                if(firstTask.getDate() == secondTask.getDate()){
-                    if(firstTask.getYear() == secondTask.getYear()){
-                        return true;
+    // Check all Tasks for any duplicates
+    private boolean checkForDuplicateReccuringTask(List<Task> tasks){
+        // Empty the List each time to avoid floooding the List
+        tasks.clear();
+        for (int i = 0; i < tasks.size(); i++) {
+            for (int j = 1; j < tasks.size(); j++) {                                       
+                if(tasks.get(i).getStartTime() == secondTask.get(j).getStartTime()){
+                    if(firstTask.get(i).getEndTime() == secondTask.get(j).getEndTime()){
+                        if(firstTask.get(i).getDate() == secondTask.get(j).getDate()){
+                            if(firstTask.get(i).getYear() == secondTask.get(j).getYear()){
+                                // Make sure the same task from the duplicate tasks is not added multiple times.
+                                if (!tasks.get(i).exists() || !tasks.get(j).exists()) reccuringTasks.add(tasks.get(i));                                     
+                                    return true;
+                            }
+                        }
                     }
                 }
             }
